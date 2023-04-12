3522bb070de218f97ce98a62fd4ee5f3f00620f3
michael.bowen <michael.bowen@mymail.champlain.edu>
2023-04-12 10:12:46 -0400

    Added links to skills

--- components/Skills.js
+++ components/Skills.js
@@ -11,9 +11,12 @@ const Skills = () => {
       <h2 className='section__title'>Skills</h2>
       <ul className='skills__list'>
         {skills.map((skill) => (
-          <li key={uniqid()} className='skills__list-item btn btn--plain'>
-            {skill}
-          </li>
+            <a href={skill.href}
+            >
+              <li key={uniqid()} className='skills__list-item btn btn--plain'>
+                  {skill.name}
+              </li>
+            </a>
         ))}
       </ul>
     </section>
